f2a562e4b61b8b79fb527fba7de80ea061ae3d32
xgeovanedamasceno <xgeovanedamasceno>
2022-01-30 13:48:34 -0300

    feat: add main container styled component
    Add main container styled component as wrapper to define a common style
    for pages

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,15 @@
 import propTypes from 'prop-types';
 import Title from '../../components/Title';
 import Subtitle from '../../components/Subtitle';
+import MainContainerStyle from '../../components/MainContainer/styled';
 
 function Home({ title, subtitle }) {
   return (
-    <>
+    <MainContainerStyle>
       <Title>{ title }</Title>
       <Subtitle>{ subtitle }</Subtitle>
-    </>
+    </MainContainerStyle>
+
   );
 }
 
