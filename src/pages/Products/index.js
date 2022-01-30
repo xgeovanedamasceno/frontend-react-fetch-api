11fabe25eeb55dd1a65371dd8cc72edfa04cd213
xgeovanedamasceno <xgeovanedamasceno>
2022-01-30 20:24:16 -0300

    feat: implement fetch data api and render it data
    Implements a fetch data to api and renders it data using useEffect and
    useState Hooks.
    
    useEffect Hook has a flag that fix tells to React when the component is
    mounted or whent its not. See item 7 on '/a-notes'

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,11 +1,42 @@
 import propTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import Title from '../../components/Title';
 import MainContainerStyle from '../../components/MainContainer/styled';
 
 function Products({ title }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+      .then((response) => response.json())
+      .then((data) => {
+        if (isMounted) setProducts(data);
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  function renderProducts() {
+    return (
+      <ul>
+        {products.map((product, index) => (
+          <li key={`${product.id}${product.id + index}`}>
+            <img src={product.fotos[0].src} alt={product.fotos[0].titulo} />
+            <h2>{product.nome}</h2>
+            <p>{product.preco}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  }
   return (
     <MainContainerStyle>
       <Title>{ title }</Title>
+      { renderProducts() }
     </MainContainerStyle>
   );
 }
