d7b6c1522bd128b517b00203134f518241d92c0f
xgeovanedamasceno <xgeovanedamasceno>
2022-01-30 13:08:33 -0300

    test: refactor test to validate nav element
    Refactor test to validate nav element in the document.
    Delete Snapshot test.
    **Snapshot tests will be create at the end of development.

--- src/components/Nav/__test__/Nav.test.js
+++ src/components/Nav/__test__/Nav.test.js
@@ -1,46 +1,23 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import renderer from 'react-test-renderer';
 import 'jest-styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import Nav from '..';
-import Anchor from '../../Anchor';
+
+function MockNav() {
+  return (
+    <BrowserRouter>
+      <Nav />
+    </BrowserRouter>
+  );
+}
 
 describe('Nav', () => {
-  it('should render an nav element with 1 anchor component', () => {
-    render(
-      <Nav>
-        <Anchor url="#exemplo" name="Products" />
-      </Nav>
-    );
-
-    const links = screen.getByRole('link', { name: 'Products' });
+  it('should render a nav element', () => {
+    render(<MockNav />);
 
     const navEl = screen.getByRole('navigation');
 
-    expect(navEl).toContainElement(links);
-  });
-
-  /* it('should render an nav element with 2 anchor components', () => {
-    render(
-      <Nav>
-        <Anchor url="#exemplo" name="Products" />
-        <Anchor url="#exemplo" name="Productz" />
-      </Nav>
-    );
-
-    const links = screen.getAllByRole('link');
-
-    const navEl = screen.getByRole('navigation');
-    expect(links).toHaveLength(1);
-  }); */
-
-  it('should render correctly', () => {
-    const tree = renderer.create(
-      <Nav>
-        <Anchor url="#exemplo" name="Products" />
-      </Nav>
-    ).toJSON();
-
-    expect(tree).toMatchSnapshot();
+    expect(navEl).toBeInTheDocument();
   });
 });
