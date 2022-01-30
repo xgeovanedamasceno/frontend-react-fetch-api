830ea47afd5f050368c1719883bbe01ecf251a61
xgeovanedamasceno <xgeovanedamasceno>
2022-01-30 11:05:14 -0300

    feat: create main container component
    This component is a wrapper to define a style of the main area of each
    page

---
+++ src/components/MainContainer/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const MainContainerStyle = styled.div`
+  background-color: aquamarine;;
+  display: flex;
+  flex-direction: row;
+  margin: 0 auto;
+  width: 750px;
+
+`;
+
+export default MainContainerStyle;
