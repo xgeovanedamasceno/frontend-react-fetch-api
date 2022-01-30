ef8ee6ccb05b436c179d70cce2672ba5ff892f6a
xgeovanedamasceno <xgeovanedamasceno>
2022-01-30 13:36:05 -0300

    feat: add anchor styled component as wrapper for Link component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
+import AnchorStyle from '../Anchor/styled';
 import NavStyle from './styled';
 
 function Nav() {
   return (
     <NavStyle>
-      <Link to="/">Home</Link>
-      <Link to="products">Products</Link>
-      <Link to="contact">Contact</Link>
+      <AnchorStyle><Link to="/">Home</Link></AnchorStyle>
+      <AnchorStyle><Link to="products">Products</Link></AnchorStyle>
+      <AnchorStyle><Link to="contact">Contact</Link></AnchorStyle>
     </NavStyle>
   );
 }
