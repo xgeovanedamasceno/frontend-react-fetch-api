b0b679cd75a33a0dbe76360a003150bb93ede352
xgeovanedamasceno <xgeovanedamasceno>
2022-02-04 18:19:02 -0300

    test: add mock request test - test fails
    Create mock request and mock response
    Create test to check to response but fails.

---
+++ src/pages/Product/__test__/Product.test.js
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom';
+import { screen, render } from '@testing-library/react';
+import Product from '..';
+import productServer from '../../../__mocks__/product-server';
+
+describe('Product', () => {
+  beforeEach(() => {
+    render(<Product />);
+  });
+
+  beforeAll(() => productServer.listen({ onUnhandledRequest: 'bypass' }));
+  afterAll(() => productServer.close());
+
+  it('should render an img', async () => {
+    const imgEl = await screen.findByRole('img');
+    screen.debug();
+    expect(imgEl).toBeInTheDocument();
+  });
+});
